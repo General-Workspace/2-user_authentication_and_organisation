2dfb6965bad0b20044649062ca019635559268a4
aniebietafia <aniebietafia87@gmail.com>
2024-07-03 13:41:46 +0100

    setting up express app

---
+++ src/app.ts
@@ -0,0 +1,78 @@
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { StatusCodes } from "http-status-codes";
+
+class App {
+  public app: express.Application;
+  //   public port: string | number;
+
+  constructor(/*controllers: any, port: string | number*/) {
+    this.app = express();
+    // this.port = port;
+
+    this.initializeMiddlewares();
+    // this.initializeControllers(controllers);
+    this.mountRoutes();
+    this.initializeErrorHandling();
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private mountRoutes() {
+    // mount route
+
+    // index route
+    this.app.get("/", (_req: Request, res: Response) => {
+      res.status(StatusCodes.OK).json({
+        message: "🚀 Welcome to this song recommendation app.",
+      });
+    });
+
+    // not found route
+    this.app.all("*", (_req: Request, res: Response) => {
+      res.status(StatusCodes.NOT_FOUND).json({
+        message: "🚫 Route not found.",
+      });
+    });
+  }
+
+  //   private initializeControllers(controllers: any) {
+  //     controllers.forEach((controller: any) => {
+  //       this.app.use("/", controller.router);
+  //     });
+  //   }
+
+  private initializeErrorHandling() {
+    this.app.use(
+      (
+        error: Error,
+        _req: Request,
+        res: Response,
+        _next: NextFunction
+      ): void => {
+        const err: {
+          name: string;
+          message: string;
+          stack?: string;
+        } = {
+          name: error.name,
+          message: error.message,
+          stack: error.stack,
+        };
+
+        console.error(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+          message: error.message,
+        });
+      }
+    );
+  }
+}
+
+export default new App().app;
