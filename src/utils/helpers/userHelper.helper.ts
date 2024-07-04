a08e7842e92b50c36072365a87e8a8b92fe75751
aniebietafia <aniebietafia87@gmail.com>
2024-07-04 16:05:29 +0100

    feat: user helper functions

--- src/utils/helpers/userHelper.helper.ts
+++ src/utils/helpers/userHelper.helper.ts
@@ -5,11 +5,11 @@ export class UserHelperFunction {
     this.fullname = fullname;
   }
 
-  get fullName(): string {
+  public get fullName(): string {
     return this.fullname;
   }
 
-  set fullName(fullName: string) {
+  public set fullName(fullName: string) {
     const parts = fullName.split(" ");
     const firstName = parts[0].charAt(0).toUpperCase() + parts[0].slice(1);
     const lastName = parts[1].charAt(0).toUpperCase() + parts[1].slice(1);
