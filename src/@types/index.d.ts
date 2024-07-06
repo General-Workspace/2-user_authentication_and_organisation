58591e1a54e24eea64ddf48fd5c5531c7256b22a
aniebietafia <aniebietafia87@gmail.com>
2024-07-06 06:02:30 +0100

    interfaces

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,31 +1,59 @@
-import { Response } from "express";
+import { Request, Response } from "express";
+import { User } from "@prisma/client";
 
 declare type ResponseData<T> = Response<T>;
 
 declare interface SuccessResponseData<T> {
-  success: boolean;
-  statusCode: number;
+  status: string;
   message: string;
   data: T;
+  statusCode: number;
 }
 
 declare interface ErrorResponseData {
-  success: boolean;
-  error: {
-    type: string;
-    message: string;
-    statusCode: number;
-  };
+  status: string;
+  message: string;
+  statusCode: number;
 }
 
 interface RegisterUserRequest {
-  id: string;
-  username: string;
-  password?: string;
-  fullname: string;
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  phone?: string | undefined;
   [key: string]: unknown;
+}
+
+interface LoginResponse {
+  userId: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone?: string;
 }
 
 declare type UserResponse = ResponseData<
   SuccessResponseData<RegisterUserRequest> | ErrorResponseData
 >;
+
+declare interface UserObject extends Request {
+  user: User;
+}
+
+// Organisation interface definitions
+declare type OrganisationResponse = ResponseData<
+  SuccessResponseData<Organisation> | ErrorResponseData
+>;
+
+declare interface Organisation {
+  orgId: string;
+  name: string;
+  description: string;
+  userId: string;
+}
+
+declare interface OrganisationObject extends Request {
+  organisation: Organisation;
+}
